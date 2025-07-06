85878fb08112469b6061cc4256f52ff358478597
Brock Salmon <bg100197@gmail.com>
2025-07-06 14:02:04 +1000

    06JUL2025: Switch to SRGB rendering, open asset packer with dir file, fix sprite rendering and alpha blending

---
+++ code/shaders/sprite_fragment.glsl
@@ -0,0 +1,21 @@
+/*
+Project: Loam
+File: sprite_fragment.glsl
+Author: Brock Salmon
+Created: 06JUL2025
+*/
+
+#version 460
+#extension GL_GOOGLE_include_directive : require
+
+#include "shared_layouts.glsl"
+
+layout (location = 0) in vec4 inColour;
+layout (location = 1) in vec2 inUV;
+
+layout (location = 0) out vec4 fragColour;
+
+void main() {   
+  fragColour = inColour * texture(colourTexture, inUV);
+}
+
