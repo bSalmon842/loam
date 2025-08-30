0b87d1c4bdd3b918727e846ad651e59c97f4a667
Brock Salmon <bg100197@gmail.com>
2025-08-30 17:09:31 +1000

    30AUG2025: Rename structs, rework pipeline layout building for multiple push constant structs

--- code/shaders/shared_layouts.glsl
+++ code/shaders/shared_layouts.glsl
@@ -6,6 +6,7 @@ Created: 04JUN2025
 */
 
 #extension GL_EXT_buffer_reference : require
+#extension GL_EXT_scalar_block_layout : require
 #extension GL_GOOGLE_include_directive : require
 //#extension GL_EXT_debug_printf : enable
 
@@ -28,3 +29,27 @@ struct CommonPushConstants {
   uint textureID;
   VertexBuffer vertexBuffer;
 };
+
+// Sprite Info
+struct SpriteInfo {
+  mat4 transform;
+  vec4 colour;
+  vec2 uv0;
+  vec2 uv1;
+  vec2 dimensions;
+  uint textureID;
+  uint padding;
+};
+
+layout (buffer_reference, scalar) readonly buffer SpriteInfoBuffer {
+  SpriteInfo spriteInfos[];
+};
+
+struct SpritePushConstants {
+  mat4 viewProj;
+  SpriteInfoBuffer spriteBuffer;
+};
+
+// Store the 6 uvs for a basic sprite so we don't need to 
+const vec2 SPRITE_BASE_UVS[] = { { 1, 1 }, { 1, 0 }, { 0, 1 }, { 0, 1 }, { 1, 0 }, { 0, 0 } };
+const vec2 SPRITE_BASE_POSITIONS[] = { { 1, 0 }, { 1, 1 }, { 0, 0 }, { 0, 0 }, { 1, 1 }, { 0, 1 } };
