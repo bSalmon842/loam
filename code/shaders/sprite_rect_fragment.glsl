f7f4d393127201c12ecb78f059538806e274a078
Brock Salmon <bg100197@gmail.com>
2025-12-18 10:19:41 +1000

    18DEC2025: (LOAM-36) Update code for new compiler version, add rect render command, pipeline, and shaders

---
+++ code/shaders/sprite_rect_fragment.glsl
@@ -0,0 +1,22 @@
+/*
+Project: Loam
+File: sprite_rect_fragment.glsl
+Author: Brock Salmon
+Created: 18DEC2025
+*/
+
+#version 460
+#extension GL_GOOGLE_include_directive : require
+
+#include "shared_layouts.glsl"
+
+layout (location = 0) in vec4 inColour;
+layout (location = 1) in vec2 inUV;
+layout (location = 2) flat in uint inTextureID;
+
+layout (location = 0) out vec4 fragColour;
+
+void main() {   
+  fragColour = inColour;
+}
+
